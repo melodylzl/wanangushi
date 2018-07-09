4c87b7f54c46c8f1d63979bea359dba7027465a6
melodylzl <290337714@qq.com>
2018-07-09 17:32:34 +0800

    [Gradle]将服务器地址在build文件中配置

--- app/src/main/java/com/byvoid/wanangushi/http/Server.java
+++ app/src/main/java/com/byvoid/wanangushi/http/Server.java
@@ -1,10 +1,13 @@
 package com.byvoid.wanangushi.http;
 
+
+import com.byvoid.wanangushi.BuildConfig;
+
 /**
  * @author melody
  * @date 2018/1/24
  */
 public class Server {
 
-    public static final String HOST_BASE = "http://10.32.8.147:6060/";
+    public static final String HOST_BASE = BuildConfig.SERVER_URL;
 }
