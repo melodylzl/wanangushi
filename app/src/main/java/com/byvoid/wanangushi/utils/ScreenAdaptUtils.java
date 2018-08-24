6091555ab4487bb6ac23c8ccdae13673929fcad3
melodylzl <290337714@qq.com>
2018-08-24 17:13:38 +0800

    [框架]修改屏幕适配工具类

--- app/src/main/java/com/byvoid/wanangushi/utils/ScreenAdaptUtils.java
+++ app/src/main/java/com/byvoid/wanangushi/utils/ScreenAdaptUtils.java
@@ -1,6 +1,7 @@
 package com.byvoid.wanangushi.utils;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.util.DisplayMetrics;
 
@@ -16,11 +17,15 @@ public class ScreenAdaptUtils {
 
 
     public static void cancelCustomDensity(@NonNull Activity activity){
+        final DisplayMetrics systemDisplayMetrics = Resources.getSystem().getDisplayMetrics();
         final DisplayMetrics appDisplayMetrics = UtilsApplication.getInstance().getResources().getDisplayMetrics();
         final DisplayMetrics activityDisplayMetrics = activity.getResources().getDisplayMetrics();
-        activityDisplayMetrics.density = appDisplayMetrics.density;
-        activityDisplayMetrics.scaledDensity = appDisplayMetrics.scaledDensity;
-        activityDisplayMetrics.densityDpi = appDisplayMetrics.densityDpi;
+        activityDisplayMetrics.density = systemDisplayMetrics.density;
+        activityDisplayMetrics.scaledDensity = systemDisplayMetrics.scaledDensity;
+        activityDisplayMetrics.densityDpi = systemDisplayMetrics.densityDpi;
+        appDisplayMetrics.density = systemDisplayMetrics.density;
+        appDisplayMetrics.scaledDensity = systemDisplayMetrics.scaledDensity;
+        appDisplayMetrics.densityDpi = systemDisplayMetrics.densityDpi;
     }
 
     public static void setCustomDensityVertical(@NonNull Activity activity, int size){
@@ -32,20 +37,20 @@ public class ScreenAdaptUtils {
     }
 
     public static void setCustomDensity(@NonNull Activity activity, int size, boolean isVertical){
+        final DisplayMetrics systemDisplayMetrics = Resources.getSystem().getDisplayMetrics();
         final DisplayMetrics appDisplayMetrics = UtilsApplication.getInstance().getResources().getDisplayMetrics();
-        float targetDensity;
-        if (isVertical){
-            targetDensity = appDisplayMetrics.widthPixels / size;
-        }else{
-            targetDensity = appDisplayMetrics.heightPixels / size;
-        }
-        final int targetDensityDpi = (int) (160 * targetDensity);
-        final float targetScaledDensity = targetDensity * (appDisplayMetrics.scaledDensity / appDisplayMetrics.density);
-
         final DisplayMetrics activityDisplayMetrics = activity.getResources().getDisplayMetrics();
-        activityDisplayMetrics.density = targetDensity;
-        activityDisplayMetrics.scaledDensity = targetScaledDensity;
-        activityDisplayMetrics.densityDpi = targetDensityDpi;
+        if (isVertical){
+            activityDisplayMetrics.density = appDisplayMetrics.widthPixels / size;
+        }else{
+            activityDisplayMetrics.density = appDisplayMetrics.heightPixels / size;
+        }
+        activityDisplayMetrics.densityDpi = (int) (160 * activityDisplayMetrics.density);
+        activityDisplayMetrics.scaledDensity = activityDisplayMetrics.density * (systemDisplayMetrics.scaledDensity / systemDisplayMetrics.density);
+
+        appDisplayMetrics.density = activityDisplayMetrics.density;
+        appDisplayMetrics.scaledDensity = activityDisplayMetrics.scaledDensity;
+        appDisplayMetrics.densityDpi = activityDisplayMetrics.densityDpi;
     }
 
 }
