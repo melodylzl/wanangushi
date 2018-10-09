ef34da33f809066cec244637aa5d6890bafd99d0
liuziliang <290337714@qq.com>
2018-10-09 11:07:39 +0800

    [故事]正式版隐藏创建故事入口

--- app/src/main/java/com/byvoid/wanangushi/module/setting/fragment/SettingFragment.java
+++ app/src/main/java/com/byvoid/wanangushi/module/setting/fragment/SettingFragment.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.byvoid.wanangushi.BuildConfig;
 import com.byvoid.wanangushi.R;
 import com.byvoid.wanangushi.module.story.activity.CreateStoryActivity;
 import com.byvoid.wanangushi.base.BaseFragment;
@@ -50,6 +51,7 @@ public class SettingFragment extends BaseFragment implements IUpdateView{
     protected void bindData() {
         super.bindData();
         mVersionTv.setText(ConfigUtils.getVersionName());
+        mCreateStoryView.setVisibility(BuildConfig.DEBUG ? View.VISIBLE : View.GONE);
     }
 
     @Override
